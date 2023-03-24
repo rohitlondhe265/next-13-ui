f9285baaad25f110a73ca5983bd788128e953a55
Rohit Londhe <rohitlondhe446@gmail.com>
2023-03-24 14:50:45 +0530

    Home page UI ready

---
+++ app/components/PostList.jsx
@@ -0,0 +1,51 @@
+export default function Footer() {
+  return (
+    <section className="text-gray-600 body-font overflow-hidden mx-3 space-y-6 md:w-1/3">
+
+      <div className="flex flex-nowrap space-x-2">
+        <div className="w-80">
+          <img
+            src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="">
+          <h2 className="text-xl font-medium text-gray-900 hover:text-primary text-justify">
+            Bitters hash waistcoat fashion axe chia unicorn
+          </h2>
+          <div className="flex justify-between text-primary"><span>Cricket</span><span>19 April, 2023</span></div>
+        </div>
+      </div>
+
+      <div className="flex flex-nowrap space-x-2">
+        <div className="w-80">
+          <img
+            src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="">
+          <h2 className="text-xl font-medium text-gray-900 hover:text-primary text-justify">
+            Bitters hash waistcoat fashion axe chia unicorn
+          </h2>
+          <div className="flex justify-between text-primary"><span>Cricket</span><span>19 April, 2023</span></div>
+        </div>
+      </div>
+
+      <div className="flex flex-nowrap space-x-2">
+        <div className="w-80">
+          <img
+            src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="">
+          <h2 className="text-xl font-medium text-gray-900 hover:text-primary text-justify">
+            Bitters hash waistcoat fashion axe chia unicorn
+          </h2>
+          <div className="flex justify-between text-primary"><span>Cricket</span><span>19 April, 2023</span></div>
+        </div>
+      </div>
+    </section>
+  );
+}
